34c1fb54c2a5e92955be89a6ccae10045f2023df
tana1024 <keiskbusiness@gmail.com>
2020-09-17 10:02:48 +0000

    pandas

--- src/script/create_prefectures.py
+++ src/script/create_prefectures.py
@@ -3,5 +3,8 @@ from ..const import (PREFECTURES_DATA_PATH)
 
 
 def create_json_file():
-    prefectures_df = pd.read_csv(PREFECTURES_DATA_PATH, encoding='utf-8')
-    print(prefectures_df)
+    df_prefectures = pd.read_csv(PREFECTURES_DATA_PATH, encoding='utf-8')
+    df_prefectures = df_prefectures.fillna({'testedPositive': 0, 'peopleTested': 0, 'hospitalized': 0, 'serious': 0, 'discharged': 0, 
+                            'deaths': 0, 'effectiveReproductionNumber': 0.0})
+    for row in df_prefectures.iterrows():
+        print(row)
