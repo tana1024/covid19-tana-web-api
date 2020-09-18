a8979e6a9d54f5b85ba23f5366772823ff5685b6
tana1024 <keiskbusiness@gmail.com>
2020-09-18 16:29:04 +0000

    prefecture implement

--- src/script/create_prefectures.py
+++ src/script/create_prefectures.py
@@ -6,8 +6,19 @@ def create_json_file():
     df_prefectures = pd.read_csv(PREFECTURES_DATA_PATH, encoding='utf-8')
     df_prefectures = df_prefectures.fillna({'testedPositive': 0, 'peopleTested': 0, 'hospitalized': 0, 'serious': 0,
                                             'discharged': 0, 'deaths': 0, 'effectiveReproductionNumber': 0.0})
-    for prefecture in PREFECTURES:
-        df_prefecture = df_prefectures[df_prefectures['prefectureNameJ'] == prefecture]
+    for id, prefecture in PREFECTURES.items():
+        list_pref = []
+
+        df_prefecture = df_prefectures.query(f'prefectureNameJ == "{prefecture}"')
+        df_prefecture = df_prefecture.reset_index(drop=True)
+        df_sort = df_prefecture.sort_values(['year', 'month', 'date'], ascending=False)
+        latest = str(df_sort.iat[0, 0]) + '/' + str(df_sort.iat[0, 1]).zfill(2) + '/' + str(df_sort.iat[0, 2]).zfill(2)
         df_sum = df_prefecture.groupby('prefectureNameJ').sum()
-        print(prefecture)
-        print(df_sum.at[prefecture, 'testedPositive'])
+        list_pref.append({
+            'id': id,
+            'latest': latest,
+            'prefectureNameJ': prefecture,
+            'prefectureNameE': df_prefecture['prefectureNameE'][0],
+            'testedPositive': df_sum['testedPositive'][0]
+        })
+        print(list_pref)
