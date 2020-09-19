0a346f72867523157132faeef0c28364d5b35cb5
tana1024 <keiskbusiness@gmail.com>
2020-09-19 09:59:30 +0000

    prefecture implement

--- src/script/create_prefectures.py
+++ src/script/create_prefectures.py
@@ -1,24 +1,32 @@
 import pandas as pd
-from ..const import (PREFECTURES_DATA_PATH, PREFECTURES)
+import json
+from ..const import (PREFECTURES_DATA_PATH, PREFECTURES, PREFECTURES_JSON_PATH)
 
 
 def create_json_file():
     df_prefectures = pd.read_csv(PREFECTURES_DATA_PATH, encoding='utf-8')
     df_prefectures = df_prefectures.fillna({'testedPositive': 0, 'peopleTested': 0, 'hospitalized': 0, 'serious': 0,
                                             'discharged': 0, 'deaths': 0, 'effectiveReproductionNumber': 0.0})
+    list_pref = []
     for id, prefecture in PREFECTURES.items():
-        list_pref = []
-
         df_prefecture = df_prefectures.query(f'prefectureNameJ == "{prefecture}"')
-        df_prefecture = df_prefecture.reset_index(drop=True)
         df_sort = df_prefecture.sort_values(['year', 'month', 'date'], ascending=False)
+        df_sort = df_sort.reset_index(drop=True)
         latest = str(df_sort.iat[0, 0]) + '/' + str(df_sort.iat[0, 1]).zfill(2) + '/' + str(df_sort.iat[0, 2]).zfill(2)
-        df_sum = df_prefecture.groupby('prefectureNameJ').sum()
         list_pref.append({
             'id': id,
             'latest': latest,
             'prefectureNameJ': prefecture,
-            'prefectureNameE': df_prefecture['prefectureNameE'][0],
-            'testedPositive': df_sum['testedPositive'][0]
+            'prefectureNameE': str(df_sort['prefectureNameE'][0]),
+            'testedPositive': str(df_sort['testedPositive'][0]),
+            'peopleTested': str(df_sort['peopleTested'][0]),
+            'hospitalized': str(df_sort['hospitalized'][0]),
+            'serious': str(df_sort['serious'][0]),
+            'discharged': str(df_sort['discharged'][0]),
+            'deaths': str(df_sort['deaths'][0]),
+            'effectiveReproductionNumber': str(df_sort['effectiveReproductionNumber'][0])
         })
-        print(list_pref)
+
+    with open(PREFECTURES_JSON_PATH, 'w', encoding='utf-8') as f:
+        json.dump(list_pref, f, indent=2, ensure_ascii=False)
+
