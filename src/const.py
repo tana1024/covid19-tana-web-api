b300fa5e9290b9eeab1e54deb3bf376b398a5629
tana1024 <keiskbusiness@gmail.com>
2020-09-22 10:37:34 +0000

    submodule add

--- src/const.py
+++ src/const.py
@@ -4,7 +4,7 @@ import os
 PREFECTURES_JSON_PATH = './created_json/prefectures.json'
 
 # source data path
-SOURCE_DATA_BASE_PATH = 'data/'
+SOURCE_DATA_BASE_PATH = 'data/master_data/data'
 PREFECTURES_DATA_PATH = os.path.join(SOURCE_DATA_BASE_PATH, 'prefectures.csv')
 
 PREFECTURES = {
